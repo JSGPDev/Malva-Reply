2077c6c2d279eac7fb1ca5e23a87de794a409b08
JSGPDev <166438846+JSGPDev@users.noreply.github.com>
2024-11-24 14:02:38 -0500

    Update Utils.js

--- Js/Utils.js
+++ Js/Utils.js
@@ -1,6 +1,6 @@
 export const getProducts = async () => {
   try {
-    const response = await fetch("./Malva-Reply/Data/data.json");
+    const response = await fetch("https://raw.githubusercontent.com/JSGPDev/Malva-Reply/refs/heads/main/Data/data.json");
     if (!response.ok) throw new Error(response.statusText);
     const data = await response.json();
     console.log(data);
